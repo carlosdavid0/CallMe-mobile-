3760b2d2d3ff81a18aa1cdf7bca82308ba438756
Carlos David <romao.carlosdavi@gmail.com>
2021-04-19 20:12:12 -0300

    mudanças

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,53 +5,6 @@ import programmer from '../assets/programmer.png';
 const data = new Date();
 
 
-function alertaTerm() {
-    Alert.alert(
-        "Opa",
-        "Para prosseguir, faça o login",
-        [
-            {
-                text: "Lembre-me depois",
-                onPress: () => console.log("Ask me later pressed")
-            },
-            {
-                text: "Cancelar",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel"
-            },
-            { text: "Ok", onPress: () => console.log("OK Pressed") }
-        ]
-    );
-}
-function alertNext() {
-    Alert.alert(
-        "Opa",
-        "Aguarde para ser direcionado a área de login",
-
-    );
-}
-
-const onShare = async () => {
-    try {
-        const result = await Share.share({
-            title: "Thiaguim do JS",
-            message:
-                'Callme é o melhor aplicativo que eu ja fiz| Enviado usando react Native',
-        });
-        if (result.action === Share.sharedAction) {
-            if (result.activityType) {
-                // shared with activity type of result.activityType
-            } else {
-                // shared
-            }
-        } else if (result.action === Share.dismissedAction) {
-            // dismissed
-        }
-    } catch (error) {
-        alert(error.message);
-    }
-};
-
 
 export default function Welcome() {
     return (
@@ -61,15 +14,10 @@ export default function Welcome() {
             <Text style={estilo.titulo}>Aproximando seus contatos profissionais </Text>
             <Image source={programmer} style={estilo.image} />
             <Text style={estilo.descricao}>Precisando otimizar as comunicações com seus colaboradores?{"\n"}<Text style={{ fontWeight: '700' }}>Dá um CallME</Text></Text>
-            <TouchableOpacity style={estilo.button} activeOpacity={0.1} onPress={alertNext}>
+            <TouchableOpacity style={estilo.button} activeOpacity={0.1}>
                 <Text style={estilo.buttonText}> Avançar </Text>
             </TouchableOpacity>
-            <TouchableOpacity style={estilo.button} activeOpacity={0.1} onPress={alertaTerm}>
-                <Text style={estilo.buttonText}> Termos de uso</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={estilo.button} activeOpacity={0.1} onPress={onShare}>
-                <Text style={estilo.buttonText}> Divulgação</Text>
-            </TouchableOpacity>
+
 
         </SafeAreaView >
 
@@ -86,10 +34,10 @@ const estilo = StyleSheet.create({
         paddingHorizontal: 15,
         color: "#fff",
         fontSize: 35,
-        marginTop: 60,
+        marginTop: 90,
     },
     image: {
-        marginTop: 20,
+        marginTop: 40,
         justifyContent: 'center',
         width: 292,
         height: 290,
